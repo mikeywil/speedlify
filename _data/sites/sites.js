71e7d5e7bd7964b59bbde80da8eb030a6427ba0b
mikeywil <michael@wilcox.net>
2020-07-15 09:34:08 +0100

    Update and rename netlify.js to sites.js
    changed to sites

---
+++ _data/sites/sites.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: "Sites", // optional, falls back to object key
+	description: "Web sites",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://whelanonsecurity.com/",
+		"https://radioquarantino.com/",
+		"https://www.kiplingsociety.co.uk/",
+		"https://www.diygogo.co.uk/",
+		"https://www.ghost.org/",
+	]
+};
